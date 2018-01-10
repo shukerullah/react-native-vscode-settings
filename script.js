bcdeb0a7a9b806cd32d6ebe0206c40e1ef291268
Pir.Shukurullah <pir.shukurullah@Pir-Shukurullah-MacMini.local>
2018-01-09 18:26:26 -0800

    Remove .eslintrc path

--- script.js
+++ script.js
@@ -1,64 +1,66 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
-const files = ['.eslintrc', '.vscode']
+const files = [".vscode"];
 
 try {
   const extend = target => {
-    var sources = [].slice.call(arguments, 1)
+    var sources = [].slice.call(arguments, 1);
     sources.forEach(function(source) {
       for (var prop in source) {
-        target[prop] = source[prop]
+        target[prop] = source[prop];
       }
-    })
-    return target
-  }
+    });
+    return target;
+  };
 
   const copyFile = (src, dest) => {
     if (fs.existsSync(dest)) {
       // Merge
-      const srcData = fs.readFileSync(src, 'utf8')
-      const destData = fs.readFileSync(dest, 'utf8')
+      const srcData = fs.readFileSync(src, "utf8");
+      const destData = fs.readFileSync(dest, "utf8");
 
-      if (destData === undefined) destData = {}
+      if (destData === undefined) destData = {};
 
-      const writeData = extend(srcData, destData)
-      console.log('write data : ' + writeData)
+      const writeData = extend(srcData, destData);
+      console.log("write data : " + writeData);
 
-      fs.writeFileSync(dest, writeData, 'utf8')
+      fs.writeFileSync(dest, writeData, "utf8");
     } else {
       // Copy
-      fs.openSync(dest, 'w')
+      fs.openSync(dest, "w");
 
-      const srcData = fs.readFileSync(src, 'utf8')
-      console.log('src data : ' + srcData)
+      const srcData = fs.readFileSync(src, "utf8");
+      console.log("src data : " + srcData);
 
-      fs.writeFileSync(dest, srcData, 'utf8')
+      fs.writeFileSync(dest, srcData, "utf8");
     }
-  }
+  };
 
   files.forEach(file => {
-    const srcPath = `${__dirname}/${file}`
-    const destPath = path.normalize(`${__dirname}/../../${file}`)
-    const stat = fs.statSync(srcPath)
+    const srcPath = `${__dirname}/${file}`;
+    const destPath = path.normalize(`${__dirname}/../../${file}`);
+    const stat = fs.statSync(srcPath);
 
     if (stat.isDirectory()) {
-        const items = fs.readdirSync(srcPath).filter(item => item !== '.DS_Store')
-      if (!fs.existsSync(destPath)) fs.mkdirSync(destPath)
+      const items = fs
+        .readdirSync(srcPath)
+        .filter(item => item !== ".DS_Store");
+      if (!fs.existsSync(destPath)) fs.mkdirSync(destPath);
 
       items.forEach(item => {
-        const itemStat = fs.statSync(`${srcPath}/${item}`)
+        const itemStat = fs.statSync(`${srcPath}/${item}`);
 
         if (itemStat.isFile()) {
-          copyFile(`${srcPath}/${item}`, `${destPath}/${item}`)
+          copyFile(`${srcPath}/${item}`, `${destPath}/${item}`);
         }
-      })
+      });
     }
 
     if (stat.isFile()) {
-      copyFile(srcPath, destPath)
+      copyFile(srcPath, destPath);
     }
-  })
+  });
 } catch (e) {
-  console.log('Prettier Pack Error : ', e)
+  console.log("Prettier Pack Error : ", e);
 }
